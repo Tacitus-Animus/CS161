b88f3df6a8ff4617460b57ba64a9be0f061c9fca
Alexander J Paul <paulalex55705@gmail.com>
2017-09-14 10:08:18 -0500

    small change to package description.

--- CS161/src/lab_3_Package/package-info.java
+++ CS161/src/lab_3_Package/package-info.java
@@ -1,10 +1,8 @@
 /**
  * <h1>Lab 3 : File IO, Arrays Package</h1>
- * Contains Monster Class and Lab 2 Main Class File
+ * Contains Monster Class and Lab 3 Main Class File
  * @author AJP
  * @version CS161
  * @since 9-SEP-17
- * @see Monster
- * @see Lab_3_Program
  */
 package lab_3_Package;
